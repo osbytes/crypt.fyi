dab61a63cbdc1475628e61bddb91db9922f77733
dillonstreator <dillonstreator@gmail.com>
2024-08-19 18:23:42 -0500

    test vault create, read, and delete

--- packages/server/src/app.test.ts
+++ packages/server/src/app.test.ts
@@ -1,6 +1,6 @@
 import pino from "pino";
 import { initApp } from "./app";
-import { initConfig } from "./config";
+import { initConfig, Config } from "./config";
 
 import tap, { Test } from "tap";
 import { Client } from "undici";
@@ -10,7 +10,8 @@ const initAppTest = async (t: Test) => {
   const config = {
     ...(await initConfig()),
     healthCheckEndpoint: "/some-health-check-endpoint",
-  };
+    vaultEntryTTLMsDefault: 1000,
+  } satisfies Config;
   const app = await initApp(config, pino({ enabled: false }));
   await app.fastify.listen();
   const baseUrl = `http://localhost:${
@@ -41,5 +42,84 @@ tap.test("app", async (t) => {
     });
 
     t.equal(response.statusCode, 200);
+  });
+
+  t.test("retrieves and burns vault entry", async (t) => {
+    const createResponse = await client.request({
+      method: "POST",
+      path: "/vault",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        c: "foobar",
+        b: true,
+        p: true,
+      }),
+    });
+
+    t.equal(createResponse.statusCode, 201);
+    const { id, dt } = (await createResponse.body.json()) as Record<
+      string,
+      unknown
+    >;
+    t.equal(typeof id, "string");
+    t.equal(typeof dt, "string");
+
+    const getResponse1 = await client.request({
+      method: "GET",
+      path: `/vault/${id}`,
+    });
+    t.equal(getResponse1.statusCode, 200);
+    const { c, b, p } = (await getResponse1.body.json()) as Record<
+      string,
+      unknown
+    >;
+    t.equal(c, "foobar");
+    t.equal(b, true);
+    t.equal(p, true);
+
+    const getResponse2 = await client.request({
+      method: "GET",
+      path: `/vault/${id}`,
+    });
+    t.equal(getResponse2.statusCode, 404);
+  });
+
+  t.test("deletes vault entry", async (t) => {
+    const createResponse = await client.request({
+      method: "POST",
+      path: "/vault",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        c: "foobar",
+        b: true,
+        p: true,
+      }),
+    });
+    const { id, dt } = (await createResponse.body.json()) as Record<
+      string,
+      unknown
+    >;
+
+    const deleteResponse = await client.request({
+      method: "DELETE",
+      path: `/vault/${id}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        dt,
+      }),
+    });
+    t.equal(deleteResponse.statusCode, 200);
+
+    const getResponse = await client.request({
+      method: "GET",
+      path: `/vault/${id}`,
+    });
+    t.equal(getResponse.statusCode, 404);
   });
 });
