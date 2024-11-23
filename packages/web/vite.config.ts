f3856261fe3b1cb5fad793637ebb1944f6f2e539
dillonstreator <dillonstreator@gmail.com>
2024-11-23 15:04:22 -0600

    web2->web

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "node:path"
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-});
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
