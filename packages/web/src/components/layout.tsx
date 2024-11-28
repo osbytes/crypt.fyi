4b3a56960dfa445804e7bf547e3aabd0ba5cbde9
dillonstreator <dillonstreator@gmail.com>
2024-11-28 14:23:56 -0600

    smaller gap between header buttons

--- packages/web/src/components/layout.tsx
+++ packages/web/src/components/layout.tsx
@@ -28,7 +28,7 @@ export function Layout() {
               </a>
             </p>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2">
             <Button
               variant="ghost"
               size="icon"
