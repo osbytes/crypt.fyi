93bf5b82ecb01f8a5a84f87e0f3363963d535777
dillonstreator <dillonstreator@gmail.com>
2025-01-01 13:21:26 -0600

    embed version in extension from package.json

--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import webExtension from 'vite-plugin-web-extension';
 import path from 'path';
+import fs from 'fs';
+
+const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 export default defineConfig({
   publicDir: 'icons',
@@ -13,7 +16,7 @@ export default defineConfig({
       manifest: () => ({
         manifest_version: 3,
         name: 'crypt.fyi',
-        version: '0.0.1',
+        version: pkg.version,
         description: 'Securely share encrypted text via crypt.fyi',
         permissions: ['contextMenus', 'clipboardWrite', 'activeTab', 'scripting', 'notifications'],
         background: {
