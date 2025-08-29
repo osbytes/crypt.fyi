fb4aa3bf32e4845a00f7cbc078a367ea80cede37
dillonstreator <dillonstreator@gmail.com>
2025-08-29 14:28:23 -0500

    allow cmd+enter to submit form

--- packages/web/src/pages/Create.tsx
+++ packages/web/src/pages/Create.tsx
@@ -683,7 +683,15 @@ export function CreatePage() {
           >
             <Card className="p-4">
               <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                <form
+                  onKeyDown={(e) => {
+                    if (e.metaKey && e.key === 'Enter') {
+                      form.handleSubmit(onSubmit)(e);
+                    }
+                  }}
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-4"
+                >
                   <FormField
                     control={form.control}
                     name="c"
